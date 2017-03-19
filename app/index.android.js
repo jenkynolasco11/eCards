738e81025ba8a6bab895ba00fb510b03a7bdbdba
Jenky Nolasco <jenky.nolasco@gmail.com>
2017-03-18 23:57:14 -0400

    initiated rn project

--- app/index.android.js
+++ app/index.android.js
@@ -2,8 +2,48 @@ import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
-  // Text,
-  // View
+  View
 } from 'react-native'
+
+const styles = StyleSheet.create({
+  main : {
+    flex : 1,
+    backgroundColor : 'steelblue',
+    justifyContent : 'center',
+    alignItems : 'center'
+  },
+  card : {
+    width : '90%',
+    height : '90%',
+    backgroundColor : 'lightgray'
+  },
+  cardHalf : {
+    flex : 1,
+    backgroundColor : 'blue'
+  }
+})
+
+const Card = props => {
+  return (
+    <View style={styles.card}>
+      <View style={[ styles.cardHalf ]}/>
+      <View style={[ styles.cardHalf, { backgroundColor : 'red' }]}/>
+    </View>
+  )
+}
+
+class app extends Component{
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <View style={styles.main}>
+        <Card />
+      </View>
+    )
+  }
+}
 
 AppRegistry.registerComponent('eCards', () => app)
