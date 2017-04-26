37e7535dce933e4b81d2fcd111b3d61d4d293f3b
Jenky Nolasco <jenky@Jenkys-Mac.local>
2017-04-25 22:20:54 -0400

    first commit, restart project

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import Koa from 'koa'
+import logger from 'koa-logger'
+import bodyparser from 'koa-bodyparser'
+
+// import koaRouter from 'koa-router'
+
+import config from './config'
+
+const app = new Koa()
+
+// configuration
+app
+  .use(logger())
+  .use(bodyparser())
+
+const auxPort = 8001
+const PORT = config.PORT || auxPort
+
+// run the server!!
+app
+  .listen( PORT, err => {
+    if(err) throw new Error(`Something happened with the server: ${ err }`)
+    console.log(`Server started at port ${ PORT }`)
+  })
